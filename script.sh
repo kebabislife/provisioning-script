08a2319a5aa86f9fec344480992bf5f9d49bec5f
Tim Nilsson <tim@milkyway>
2025-02-15 11:50:10 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,64 +1,100 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+# Activate virtual environment
 source /venv/main/bin/activate
-COMFYUI_DIR=${WORKSPACE}/ComfyUI/
-CHECKPOINTS_DIR=${WORKSPACE}/ComfyUI/models/checkpoints
-UPSCALE_MODELS_DIR=${WORKSPACE}/ComfyUI/models/upscale_models
-CUSTOM_NODES_DIR=${WORKSPACE}/ComfyUI/custom_nodes
-CLIP_VISION_DIR=${WORKSPACE}/ComfyUI/models/clip_vision
-INPAINT_DIR=${WORKSPACE}/ComfyUI/models/inpaint
-IPADAPTER_DIR=${WORKSPACE}/ComfyUI/models/ipadapter
-LORAS_DIR=${WORKSPACE}/ComfyUI/models/loras
 
-cd $COMFYUI_DIR
+# Define directories
+COMFYUI_DIR=${WORKSPACE}/ComfyUI
+MODELS_DIR=${COMFYUI_DIR}/models
+CUSTOM_NODES_DIR=${COMFYUI_DIR}/custom_nodes
 
-echo CIVITAI_TOKEN:
-echo $CIVITAI_TOKEN
+# Define subdirectories
+CHECKPOINTS_DIR=${MODELS_DIR}/checkpoints
+UPSCALE_MODELS_DIR=${MODELS_DIR}/upscale_models
+CLIP_VISION_DIR=${MODELS_DIR}/clip_vision
+INPAINT_DIR=${MODELS_DIR}/inpaint
+IPADAPTER_DIR=${MODELS_DIR}/ipadapter
+LORAS_DIR=${MODELS_DIR}/loras
 
-# Uninstall existing packages
-pip uninstall -y torch torchvision torchaudio pyaudio
+# Ensure directories exist
+mkdir -p "$CHECKPOINTS_DIR" "$UPSCALE_MODELS_DIR" "$CLIP_VISION_DIR" \
+         "$INPAINT_DIR" "$IPADAPTER_DIR" "$LORAS_DIR"
 
-# Install nightly builds of torch, torchvision, and torchaudio with CUDA 12.8 support
-pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cu128
-pip install opencv-python
+# Function to install packages
+install_packages() {
+    PytorchVersion=${PYTORCH_VERSION:-"--pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cu128"}
+    pip uninstall -y torch torchvision torchaudio pyaudio
+    pip install $PytorchVersion
+    pip install opencv-python
+}
 
-cd $CHECKPOINTS_DIR
-# wget https://civitai.com/api/download/models/1411338?token=$CIVITAI_TOKEN --content-disposition # Illustrious XL Cyberfix
-# wget https://civitai.com/api/download/models/1413730?token=$CIVITAI_TOKEN --content-disposition # Animagine XL 4.0 opt-perp cyberfix v2
-# wget https://civitai.com/api/download/models/1410435?token=$CIVITAI_TOKEN --content-disposition # WAI-NSFW-illustrious-SDXL v11.0
-wget https://civitai.com/api/download/models/1404800?token=$CIVITAI_TOKEN --content-disposition # Hesperides v1.0
+# Function to download files
+fetch_file() {
+    local url=$1
+    local dest_dir=$2
+    local filename=$3
+    mkdir -p "$dest_dir"
+    cd "$dest_dir"
+    if [[ "$url" == *"civitai.com"* ]]; then
+        wget "$url?token=$CIVITAI_TOKEN" --content-disposition -O "$filename"
+    else
+        wget "$url" --content-disposition -O "$filename"
+    fi
+}
 
-cd $CUSTOM_NODES_DIR
-git clone https://github.com/Acly/comfyui-tooling-nodes.git
-git clone https://github.com/Acly/comfyui-inpaint-nodes.git
-git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git
-cd comfyui_controlnet_aux
-pip install -r requirements.txt
+# Function to clone Git repositories
+clone_repo() {
+    local repo_url=$1
+    local dest_dir=$2
+    mkdir -p "$dest_dir"
+    cd "$dest_dir"
+    git clone "$repo_url" || echo "Repository $repo_url already exists"
+}
 
-cd $CUSTOM_NODES_DIR
-git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus.git
+# Install necessary packages
+install_packages
 
-cd $CLIP_VISION_DIR
-wget https://huggingface.co/h94/IP-Adapter/resolve/main/models/image_encoder/model.safetensors -O clip-vision_vit-h.safetensors --content-disposition
+# Download model checkpoints from environment variables
+echo "Downloading model checkpoints..."
+for var in $(printenv | grep '^CHECKPOINT_URL_' | awk -F= '{print $1}'); do
+    url=${!var}
+    index=$(echo "$var" | awk -F_ '{print $3}')
+    filename_var="CHECKPOINT_FILENAME_$index"
+    filename=${!filename_var:-$(basename "$url")}
+    fetch_file "$url" "$CHECKPOINTS_DIR" "$filename"
+done
 
-cd $UPSCALE_MODELS_DIR
-wget https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_NMKD-YandereNeoXL_200k.pth --content-disposition
-wget https://huggingface.co/gemasai/4x_NMKD-Superscale-SP_178000_G/resolve/main/4x_NMKD-Superscale-SP_178000_G.pth -O 4x_NMKD-Superscale-SP_178000_G.pth --content-disposition
-wget https://huggingface.co/Acly/Omni-SR/resolve/main/OmniSR_X2_DIV2K.safetensors -O OmniSR_X2_DIV2K.safetensors --content-disposition
-wget https://huggingface.co/Acly/Omni-SR/resolve/main/OmniSR_X3_DIV2K.safetensors -O OmniSR_X3_DIV2K.safetensors --content-disposition
-wget https://huggingface.co/Acly/Omni-SR/resolve/main/OmniSR_X4_DIV2K.safetensors -O OmniSR_X4_DIV2K.safetensors --content-disposition
+# Clone custom nodes repositories
+clone_repo "https://github.com/Acly/comfyui-tooling-nodes.git" "$CUSTOM_NODES_DIR"
+clone_repo "https://github.com/Acly/comfyui-inpaint-nodes.git" "$CUSTOM_NODES_DIR"
+clone_repo "https://github.com/cubiq/ComfyUI_IPAdapter_plus.git" "$CUSTOM_NODES_DIR"
+clone_repo "https://github.com/Fannovel16/comfyui_controlnet_aux.git" "$CUSTOM_NODES_DIR"
 
-mkdir -p $IPADAPTER_DIR
-cd $IPADAPTER_DIR
-wget https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl_vit-h.safetensors -O ip-adapter_sdxl_vit-h.safetensors --content-disposition
+# Install requirements for controlnet_aux
+cd "$CUSTOM_NODES_DIR/comfyui_controlnet_aux" && pip install -r requirements.txt
 
-cd $LORAS_DIR
-wget https://huggingface.co/ByteDance/Hyper-SD/resolve/main/Hyper-SDXL-8steps-CFG-lora.safetensors -O Hyper-SDXL-8steps-CFG-lora.safetensors --content-disposition
-wget https://huggingface.co/ByteDance/Hyper-SD/resolve/main/Hyper-SDXL-8steps-lora.safetensors --content-disposition
+# Download additional models
+fetch_file "$CLIP_VISION" "$CLIP_VISION_DIR" "clip-vision_vit-h.safetensors"
 
-mkdir -p $INPAINT_DIR
-cd $INPAINT_DIR
-wget https://huggingface.co/lllyasviel/fooocus_inpaint/resolve/main/fooocus_inpaint_head.pth -O fooocus_inpaint_head.pth --content-disposition
-wget https://huggingface.co/lllyasviel/fooocus_inpaint/resolve/main/inpaint_v26.fooocus.patch -O inpaint_v26.fooocus.patch --content-disposition
-wget https://huggingface.co/Acly/MAT/resolve/main/MAT_Places512_G_fp16.safetensors -O MAT_Places512_G_fp16.safetensors --content-disposition
+# Download upscaling models
+fetch_file "$UPSCALE_1" "$UPSCALE_MODELS_DIR" "4x_NMKD-YandereNeoXL_200k.pth"
+fetch_file "$UPSCALE_2" "$UPSCALE_MODELS_DIR" "4x_NMKD-Superscale-SP_178000_G.pth"
+fetch_file "$UPSCALE_3" "$UPSCALE_MODELS_DIR" "OmniSR_X2_DIV2K.safetensors"
+fetch_file "$UPSCALE_4" "$UPSCALE_MODELS_DIR" "OmniSR_X3_DIV2K.safetensors"
+fetch_file "$UPSCALE_5" "$UPSCALE_MODELS_DIR" "OmniSR_X4_DIV2K.safetensors"
 
+# Download IPAdapter models
+fetch_file "$IPADAPTER" "$IPADAPTER_DIR" "ip-adapter_sdxl_vit-h.safetensors"
+
+# Download LORA models
+fetch_file "$LORA_1" "$LORAS_DIR" "Hyper-SDXL-8steps-CFG-lora.safetensors"
+fetch_file "$LORA_2" "$LORAS_DIR" "Hyper-SDXL-8steps-lora.safetensors"
+
+# Download Inpaint models
+fetch_file "$INPAINT_1" "$INPAINT_DIR" "fooocus_inpaint_head.pth"
+fetch_file "$INPAINT_2" "$INPAINT_DIR" "inpaint_v26.fooocus.patch"
+fetch_file "$INPAINT_3" "$INPAINT_DIR" "MAT_Places512_G_fp16.safetensors"
+
+echo "Setup completed successfully!"
