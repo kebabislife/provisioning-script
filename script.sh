526454c0ce9a1c3e4e956f0fb1335a2a34d0d03b
Tim Nilsson <tim@milkyway>
2025-02-15 12:23:28 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,9 +24,8 @@ mkdir -p "$CHECKPOINTS_DIR" "$UPSCALE_MODELS_DIR" "$CLIP_VISION_DIR" \
 
 # Function to install packages
 install_packages() {
-    PytorchVersion=${PYTORCH_VERSION:-"--pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cu128"}
-    pip uninstall -y torch torchvision torchaudio pyaudio
-    pip install $PytorchVersion
+    pip uninstall -y torch torchvision torchaudio
+    pip install pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cu128
     pip install opencv-python
 }
 
